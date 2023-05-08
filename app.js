d2ebdc14b8381550dccccddbc0092d5a48b2c3a3
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-05-08 07:02:33 +0300

    firts commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("You must enter a value");
+    }
+       else{
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"; 
+        li.appendChild(span)
+       
+    }
+    inputBox.value =""; // inputbox yazı yazıldıktan sonra boşaltır 
+    saveData(); // yazılan taskleri sayfa yenilense dahi liste de tutar
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData();
+    }
+      else if(e.target.tagName === "SPAN"){
+       e.target.parentElement.remove();
+       saveData()
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}  
+function showTask(){
+listContainer.innerHTML=localStorage.getItem("data")
+}
+showTask();
